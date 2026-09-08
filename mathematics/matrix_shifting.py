3a130575f047534da5e9f3e4e9d5674b4e597d44
ErenYazgan <erenharden@outlook.com>
2026-09-08 15:15:38 +0300

    feat:translated to english

--- mathematics/matrix_shifting.py
+++ mathematics/matrix_shifting.py
@@ -7,10 +7,10 @@ A = np.round(10 * (A_raw.T @ A_raw))
 A[:, 0] = A[:, 1]
 
 print("Expected Capacity: 30")
-print("New rank after sabotage:", np.linalg.matrix_rank(A)) # Çıktı 29 olacak (Sistem Rank-Deficient)
+print("New rank after sabotage:", np.linalg.matrix_rank(A)) 
 
 lmbda = 0.01
 
 B = A + (lmbda * np.eye(m))
 
-print("New rank after shifting:", np.linalg.matrix_rank(B)) # Çıktı tekrar 30 olacak (Full-Rank)
+print("New rank after shifting:", np.linalg.matrix_rank(B)) 
